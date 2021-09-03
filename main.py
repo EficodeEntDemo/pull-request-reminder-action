011d90dff520da82860d976197341d1abdf7539a
Adam Chabin <adam.chabin@eficode.com>
2021-09-03 11:25:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def get_pull_request_page(url, token):
 def main():
     repo = os.environ["GITHUB_REPOSITORY"]
     server_url = os.environ["GITHUB_SERVER_URL"]
-    github_token = os.environ["github_token"]
+    github_token = os.environ["INPUT_GITHUB_TOKEN"]
 
     print(f"Hello repo: {server_url}/{repo}")
     print("Hello repo: %s", github_token[0:4])
